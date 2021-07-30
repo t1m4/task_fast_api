aacf5af70e3048e87d858a67703673711e6e75b6
t1m4 <50275532+t1m4@users.noreply.github.com>
2021-07-30 17:00:09 +0300

    Delete is_superuser from User model

--- db/models/user.py
+++ db/models/user.py
@@ -9,6 +9,5 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean(), default=False)
     items = relationship("Item", back_populates="owner")
 
