f7b1a59acb6ee0a92dc8b63fdc780f5fbad9f280
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-02 11:39:35 +0300

    Add ondelete cascade

--- db/models/item.py
+++ db/models/item.py
@@ -7,5 +7,5 @@ from db.base_classes import Base
 class Item(Base):
     title = Column(String, index=True)
     description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
     owner = relationship("User", back_populates="items")
